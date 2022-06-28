5aa67f684ff9d484cabe8e35c362a18cc95c7957
developdestroyer <developdestroyer@gmail.com>
2022-06-28 23:04:39 +0900

    feat: 키값 분리

--- backend/src/main/java/com/carrot/trip/controller/TouristAttractionController.java
+++ backend/src/main/java/com/carrot/trip/controller/TouristAttractionController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,8 @@ public class TouristAttractionController {
 
     private final OpenAPIService openAPIService;
 
-    @GetMapping("/touristAttraction")
-    public LocationOpenApiResponseDTO getMapData() throws URISyntaxException, JsonProcessingException {
-        return openAPIService.openAPICall();
+    @GetMapping("/touristAttraction/list/x/{x}/y/{y}")
+    public LocationOpenApiResponseDTO getMapData(@PathVariable("x") Double x, @PathVariable("y") Double y) throws URISyntaxException, JsonProcessingException {
+        return openAPIService.openAPICall(x, y);
     }
 }
