c0314c75b43ee478b6a22b9d6f18ca09e5a81769
jungmin.sung <ivsinji746@naver.com>
2022-07-01 02:52:26 +0900

    redux add

---
+++ frontend/src/redux/signupInfo/selector.ts
@@ -0,0 +1,10 @@
+import { RootState } from 'redux/rootReducer';
+
+const singupInfo1 = (state: RootState) => state.signupInfo.data.signupInfo1;
+const singupInfo2 = (state: RootState) => state.signupInfo.data.signupInfo2;
+const singupInfo3 = (state: RootState) => state.signupInfo.data.signupInfo3;
+const singupInfo4 = (state: RootState) => state.signupInfo.data.signupInfo4;
+const singupInfo5 = (state: RootState) => state.signupInfo.data.signupInfo5;
+const singupInfo6 = (state: RootState) => state.signupInfo.data.signupInfo6;
+
+export { singupInfo1, singupInfo2, singupInfo3, singupInfo4, singupInfo5, singupInfo6 };
---
+++ frontend/src/redux/signupInfo/thunk.ts
@@ -0,0 +1,47 @@
+import { ThunkAction } from 'redux-thunk';
+import { RootState } from 'redux/rootReducer';
+import { CombinedSignupData, SignupInfo1Data } from 'vo/signup';
+import {
+    signupInfo1UpdateAction,
+    signupInfo2UpdateAction,
+    signupInfo3UpdateAction,
+    signupInfo4UpdateAction,
+    signupInfo5UpdateAction,
+    signupInfo6UpdateAction,
+} from './actions';
+import { SignupOnboardAction } from './reducer';
+
+export const updateInfo1 = (
+    data: CombinedSignupData<SignupInfo1Data>,
+): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo1UpdateAction(data));
+    };
+};
+
+export const updateInfo2 = (data: CombinedSignupData<any>): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo2UpdateAction(data));
+    };
+};
+export const updateInfo3 = (data: CombinedSignupData<any>): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo3UpdateAction(data));
+    };
+};
+export const updateInfo4 = (data: CombinedSignupData<any>): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo4UpdateAction(data));
+    };
+};
+export const updateInfo5 = (data: CombinedSignupData<any>): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo5UpdateAction(data));
+    };
+};
+
+export const updateInfo6 = (data: CombinedSignupData<any>): ThunkAction<void, RootState, null, SignupOnboardAction> => {
+    return async (dispatch) => {
+        await dispatch(signupInfo6UpdateAction(data));
+    };
+};
