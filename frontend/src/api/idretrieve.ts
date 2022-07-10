5ae83d33a612a6b450afd9a886e7e58b12fe7767
jungmin.sung <ivsinji746@naver.com>
2022-07-10 23:59:06 +0900

    update log need to fix other data

--- frontend/src/api/idretrieve.ts
+++ frontend/src/api/idretrieve.ts
@@ -1,7 +1,7 @@
 import { restGet } from 'common/http';
 import { IdRetrieveReq } from 'vo/signup';
 
-export const ID_EXIST_URL: string = '/api/join/isExistNickname';
+export const ID_EXIST_URL: string = 'localhost:8080/api/join/isExistNickname';
 
 export const getUserExist = async (username: string): Promise<boolean> => {
     const response = await restGet<IdRetrieveReq, boolean>(ID_EXIST_URL, {
