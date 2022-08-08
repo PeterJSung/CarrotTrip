5289a8bc09deb37175e02f65fa873eab5e4a3ec0
developdestroyer <developdestroyer@gmail.com>
2022-08-08 20:03:47 +0900

    feat : 로그인 시 개인 취향정보도 반환해주도록 개선

--- backend/src/main/java/com/carrot/trip/dto/TokenDTO.java
+++ backend/src/main/java/com/carrot/trip/dto/TokenDTO.java
@@ -1,9 +1,12 @@
 package com.carrot.trip.dto;
 
+import com.carrot.trip.entity.MemberTaste;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
 
 @Builder
 @Getter
@@ -13,4 +16,5 @@ public class TokenDTO {
     private String token;
     private String nickname;
     private String mbti;
+    private ArrayList<MemberTaste> tasteList;
 }
